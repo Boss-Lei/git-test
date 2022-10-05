0ed809adb6548e6c46673c3018d55a2fcf5f9031
Boss-Lei <38515653+Boss-Lei@users.noreply.github.com>
2022-10-05 12:48:52 +0800

    浏览器修改

--- src/main/java/com/lei/git/GitTest.java
+++ src/main/java/com/lei/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("master commit2");
         System.out.println("hot-fix commit1");
         System.out.println("push commit1");
+        System.out.println("browed commit");
     }
 }
