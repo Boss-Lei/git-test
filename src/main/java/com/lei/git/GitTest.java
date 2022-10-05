5d2c14ef321b461750ee54abf3ec3766db9cb752
Lei <15566941591@139.com>
2022-10-05 08:44:32 +0800

    third commit

--- src/main/java/com/lei/git/GitTest.java
+++ src/main/java/com/lei/git/GitTest.java
@@ -6,5 +6,6 @@ package com.lei.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
