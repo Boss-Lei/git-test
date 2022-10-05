282d18c6941204fe9fd3b838b467eb9e68ae0d61
Lei <15566941591@139.com>
2022-10-05 12:36:50 +0800

    push commit1

--- src/main/java/com/lei/git/GitTest.java
+++ src/main/java/com/lei/git/GitTest.java
@@ -8,6 +8,8 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("master commit2");
         System.out.println("hot-fix commit1");
+        System.out.println("push commit1");
     }
 }
